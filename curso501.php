9b2150b157c658a25b8f06eb63dfee255c7ce96d
root <root@webservercloud.dexter.com.br>
2015-01-12 19:48:55 -0200

    Commit Inicial do Curso

---
+++ curso501.php
@@ -0,0 +1,15 @@
+<?php
+
+
+/* 1. Orientacao a  Objeto
+
+Utilizacao de componentes individuais.
+
+
+Classes: Arquitetura do Objeto
+
+Objeto: Apos a elaboracao da arquiterura o item final.
+
+
+
+
